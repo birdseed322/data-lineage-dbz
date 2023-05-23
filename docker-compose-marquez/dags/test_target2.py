d272111d8db2cda17386a1e8b27c27188855b57e
Ezekiel <ezekielang@protonmail.com>
2023-05-23 16:19:55 +0800

    Added sample airflow dags test cases

---
+++ docker-compose-marquez/dags/test_target2.py
@@ -0,0 +1,19 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+
+@dag(start_date=datetime(2023, 1, 1), schedule='@daily', catchup=False)
+def test_target2():
+
+    @task
+    def start(dag_run=None):
+        print(dag_run.conf.get("message"))
+        return "Trigger DAG"
+
+    @task
+    def done():
+        print("done")
+    
+    start() >> done()
+
+test_target2()
