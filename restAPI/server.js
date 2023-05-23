989ef029ef65de79145d3a5b75207c55cc4e828a
Wesley Teo <wesley.tyx@gmail.com>
2023-05-23 10:01:40 +0800

    Added server.js

--- restAPI/server.js
+++ restAPI/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const neo4j = require('neo4j-driver');
+
+var driver = neo4j.driver(
+  'bolt://localhost',
+  neo4j.auth.basic('neo4j', 'neo4jneo4j')
+);
+
+var session = driver.session();
+
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  session.run('MATCH(n:Movie) RETURN n LIMIT 25')
+    .then(function(result) {
+      result.records.forEach(function(record){
+        console.log(record._fields[0].properties);
+      })
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+  res.send('Entry page');
 })
+
 
 app.post('/test', function(req, res){
   console.log("Received at /test endpoint");
@@ -24,6 +43,6 @@ app.post('/api/v1/lineage', function(req, res){
   res.send('ok');
 }) 
 
-app.listen(3000, function() {
-  console.log("Server is now running on port 3000");
+app.listen(3001, function() {
+  console.log("Server is now running on port 3001");
 });
