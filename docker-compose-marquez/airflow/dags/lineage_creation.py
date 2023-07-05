c2356796c172d3452cf50d34751f55f552d43f3e
Wesley Teo <wesley.tyx@gmail.com>
2023-07-05 14:07:00 +0800

    Implemented hashmap to track DAGs which lineages have been created

--- docker-compose-marquez/airflow/dags/lineage_creation.py
+++ docker-compose-marquez/airflow/dags/lineage_creation.py
@@ -1,10 +1,10 @@
 from airflow import DAG
-from airflow.utils.dates import days_ago
-from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
-
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.utils.dates import days_ago
 import requests
 import base64
+
 airflow_backend = "http://airflow:8080/api/v1/"
 airflow_user = "airflow"
 airflow_password = "airflow"
@@ -12,13 +12,14 @@ headers =  {
   "Authorization": "Basic " + base64.b64encode(str.encode(f'{airflow_user}:{airflow_password}')).decode('utf-8'),
   "Content-type": "application/json"
 }
+
 default_args = {
     'owner': 'dbz',
     'depends_on_past': False,
     'start_date': days_ago(1),
     'email_on_failure': False,
     'email_on_retry': False,
-    'email': ['datascience@example.com']
+    'email': ['datascience@example.com'],
 }
 
 with DAG(
@@ -30,24 +31,38 @@ with DAG(
     description='DAG that generates lineage creation of all other DAGs in Neo4j.'
 ) as dag:
 
-    def test():
-      response = requests.get(f"{airflow_backend}dags",headers=headers)
+    def get_dag_ids(**context):
+      try: 
+        dictionary={}
+        response = requests.get(f"{airflow_backend}dags",headers=headers)
+        data = response.json()
+        for dag in data["dags"]:
+          key = dag["dag_id"]
+          dictionary[key] = False
+        context['ti'].xcom_push(key='dictionary', value=dictionary)
+        for key in dictionary:
+          if dictionary[key] == False:
+            print("Go through function")
+            dummy_recursive_function(key, **context)
+          else:
+            print("skipped DAG")
+        dictionary = context['ti'].xcom_pull(key='dictionary')
+        print(dictionary) 
+      except requests.exceptions.RequestException as err:
+        print("Error occurred during DAGs retrieval:", err)
 
-    t1 = PythonOperator(task_id='test', python_callable=test)
+    get_dag_ids = PythonOperator(task_id='get_dag_ids', python_callable=get_dag_ids)
 
-      # if response.status_code == 200:
-      # data = response.json()
-      # dictionary = {}
-      # for dag in data["dags"]:
-      #   key = dag["dag_id"]
-      #   value = False
-      #   dictionary[key] = value
-      
-      # print(dictionary)
+    def dummy_recursive_function(dag_id, **context):
+      try:
+        dictionary = context['ti'].xcom_pull(key='dictionary')
+        dictionary[dag_id] = True
+        context['ti'].xcom_push(key='dictionary', value=dictionary)
+        response = requests.get(f"{airflow_backend}dags/{dag_id}/tasks",headers=headers)
+        data = response.json()
+        for task in data.tasks:
+          
+      except requests.exceptions.RequestException as err:
+        print("Error occurred during lineage creation", err)     
 
-    def start():
-      print("start")
-
-    t2 = PythonOperator(task_id='start', python_callable=start)
-
-    t1 >> t2
+    get_dag_ids
